8eac4e5bfd1b5b4f484e55d0c7bb13ec850e7553
Daleb015 <daleb015@gmail.com>
2020-11-28 02:07:23 -0500

    Se configuran colores en el controlador de sockets

--- src/main/java/co/com/daleb/springbackendchat/controller/ChatController.java
+++ src/main/java/co/com/daleb/springbackendchat/controller/ChatController.java
@@ -1,22 +1,34 @@
 package co.com.daleb.springbackendchat.controller;
 
-import co.com.daleb.springbackendchat.model.documents.Mensaje;
+import co.com.daleb.springbackendchat.model.Mensaje;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
+
+    private String[] colores = {"red","green","blue","brown","pink"};
+
     @MessageMapping("/mensaje")
     @SendTo("/chat/mensaje")
     public Mensaje recibeMensaje(Mensaje mensaje){
+
     mensaje.setFecha(new Date().getTime());
     mensaje.setTexto(mensaje.getTexto());
     if (mensaje.getTipo().equals("NUEVO_USUARIO")){
         mensaje.setTexto(" Nuevo usuario conectado");
+        mensaje.setColor(colores[new Random().nextInt(colores.length)]);
     }
     return mensaje;
+    }
+
+    @MessageMapping("/escribiendo")
+    @SendTo("/chat/escribiendo")
+    public String escribiendo(String username){
+        return username.concat(" está escribieno");
     }
 }
